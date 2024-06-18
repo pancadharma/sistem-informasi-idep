d59144e2dd908a599022c5882a6a8d1c82300237
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-06-18 16:52:05 +0800

    update layout

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,29 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/profile', function(){
-    return view('layouts.app');
-});
-
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Auth::routes();
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/home', function() {
+Route::get('/dashboard', function () {
     return view('home');
-})->name('home')->middleware('auth');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-// Route::get('/home', function () {
-//     if (session('status')) {
-//         return redirect()->route('admin.home')->with('status', session('status'));
-//     }
-
-//     return redirect()->route('admin.home');
-// });
+require __DIR__.'/auth.php';
