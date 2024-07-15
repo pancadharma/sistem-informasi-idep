ec295d855408b6cfe1fa8f05ce7d1789d083883f
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-07-15 17:34:48 +0800

    #11

--- project/database/migrations/2024_07_15_152523_create_kabupaten_table.php
+++ project/database/migrations/2024_07_15_152523_create_kabupaten_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('kabupaten', function (Blueprint $table) {
             $table->id();
             $table->string('kode',15);
+            $table->string('type',15)->default('Kabupaten');
             $table->string('nama', 200);
             // Foreign key constraint with cascade delete
             $table->foreignId('provinsi_id')->constrained('provinsi')->onDelete('cascade');
