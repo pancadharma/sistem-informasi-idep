064556c24e8359c82a391b0deb42703985c53ca1
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-09-20 23:46:32 +0800

    #24 - init

--- project/app/Http/Controllers/Admin/RolesController.php
+++ project/app/Http/Controllers/Admin/RolesController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Gate;
 
 class RolesController extends Controller
 {
-    //
+    public function index(){
+        abort_if(Gate::denies('user_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        $roles = Role::pluck('nama', 'id');
+        return view('master.users.index', compact('roles'));
+    }
 }
