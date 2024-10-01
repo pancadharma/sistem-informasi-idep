f2d5d5b6fb8e974dd201ff1634a6bf9fc5e9d531
panca <wayanpanca@sanatasystem.com>
2024-10-01 20:14:14 +0800

    #53 #migtration table mkategorilokasikegiatan

---
+++ project/app/Models/Kategori_Lokasi_Kegiatan.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use DateTimeInterface;
+use App\Traits\Auditable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Kategori_Lokasi_Kegiatan extends Model
+{
+    protected $table = 'mkategorilokasikegiatan';
+
+    protected $fillable = [
+        'nama',
+        'aktif',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
