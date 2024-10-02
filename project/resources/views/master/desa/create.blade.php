4f2036175ac38e9171742bd4132a3819d1198de5
Wira <yanwirawan33@gmail.com>
2024-10-02 22:28:23 +0800

    #52 - fix conflict

--- project/resources/views/master/desa/create.blade.php
+++ project/resources/views/master/desa/create.blade.php
@@ -60,7 +60,7 @@
             <strong>{{ trans('cruds.status.title') }}</strong>
                 <div class="icheck-primary">
                     <input type="checkbox" name="aktif" id="aktif" {{ old('aktif',1) == 1 ? 'checked' : '' }} value="1">
-                    <label for="aktif">{{ trans('cruds.status.aktif') }}</label>
+                    <label for="aktif">{{ __('cruds.status.aktif') }}</label>
                 </div>
             </div>
             <button class="btn btn-success float-right btn-add-desa" data-toggle="tooltip" data-placement="top" type="submit" title="{{ trans('global.save') }}"><i class="fas fa-save"></i> {{ trans('global.save') }}</button>
