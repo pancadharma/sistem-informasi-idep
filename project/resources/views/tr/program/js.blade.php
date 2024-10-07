c57fb6f5308f0948cc3b6d0f09b06f750e6ea83a
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-10-07 12:14:10 +0800

    #41 - disable scrollX to make table responsive

--- project/resources/views/tr/program/js.blade.php
+++ project/resources/views/tr/program/js.blade.php
@@ -103,66 +103,66 @@
 </script>
 
 <script>
-    var uploadedFilePendukungMap = {}
-    Dropzone.options.filePendukungDropzone = {
-        url: "{{ route('program.storeMedia') }}",
-        maxFilesize: 2, // MB
-        addRemoveLinks: true,
-        headers: {
-            'X-CSRF-TOKEN': "{{ csrf_token() }}"
-        },
-        params: {
-            size: 2
-        },
-        success: function (file, response) {
-            $('form').append('<input type="hidden" name="file_pendukung[]" value="' + response.name + '">')
-            uploadedFilePendukungMap[file.name] = response.name
-        },
-        removedfile: function (file) {
-        file.previewElement.remove()
-        var name = ''
-        if (typeof file.file_name !== 'undefined') {
-            name = file.file_name
-        } else {
-            name = uploadedFilePendukungMap[file.name]
-        }
-        $('form').find('input[name="file_pendukung[]"][value="' + name + '"]').remove()
-        },
-        init: function () {
-            @if(isset($program) && $program->file_pendukung)
-            var files =
-                {!! json_encode($program->file_pendukung) !!}
-                for (var i in files) {
-                var file = files[i]
-                this.options.addedfile.call(this, file)
-                file.previewElement.classList.add('dz-complete')
-                $('form').append('<input type="hidden" name="file_pendukung[]" value="' + file.file_name + '">')
-                }
-    @endif
-        },
-        error: function (file, response) {
-            if ($.type(response) === 'string') {
-                var message = response //dropzone sends it's own error messages in string
-            } else {
-                var message = response.errors.file
-            }
-            file.previewElement.classList.add('dz-error')
-            _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
-            _results = []
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                node = _ref[_i]
-                _results.push(node.textContent = message)
-            }
+    // var uploadedFilePendukungMap = {}
+    // Dropzone.options.filePendukungDropzone = {
+    //     url: "{{ route('program.storeMedia') }}",
+    //     maxFilesize: 2, // MB
+    //     addRemoveLinks: true,
+    //     headers: {
+    //         'X-CSRF-TOKEN': "{{ csrf_token() }}"
+    //     },
+    //     params: {
+    //         size: 2
+    //     },
+    //     success: function (file, response) {
+    //         $('form').append('<input type="hidden" name="file_pendukung[]" value="' + response.name + '">')
+    //         uploadedFilePendukungMap[file.name] = response.name
+    //     },
+    //     removedfile: function (file) {
+    //     file.previewElement.remove()
+    //     var name = ''
+    //     if (typeof file.file_name !== 'undefined') {
+    //         name = file.file_name
+    //     } else {
+    //         name = uploadedFilePendukungMap[file.name]
+    //     }
+    //     $('form').find('input[name="file_pendukung[]"][value="' + name + '"]').remove()
+    //     },
+    //     init: function () {
+    //         @if(isset($program) && $program->file_pendukung)
+    //         var files =
+    //             {!! json_encode($program->file_pendukung) !!}
+    //             for (var i in files) {
+    //             var file = files[i]
+    //             this.options.addedfile.call(this, file)
+    //             file.previewElement.classList.add('dz-complete')
+    //             $('form').append('<input type="hidden" name="file_pendukung[]" value="' + file.file_name + '">')
+    //             }
+    // @endif
+    //     },
+    //     error: function (file, response) {
+    //         if ($.type(response) === 'string') {
+    //             var message = response //dropzone sends it's own error messages in string
+    //         } else {
+    //             var message = response.errors.file
+    //         }
+    //         file.previewElement.classList.add('dz-error')
+    //         _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
+    //         _results = []
+    //         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    //             node = _ref[_i]
+    //             _results.push(node.textContent = message)
+    //         }
 
-            return _results
-        }
-    }
+    //         return _results
+    //     }
+    // }
 
 // Data Table
 $(document).ready(function() {
         $('#program-list').DataTable({
             responsive: true,
-            scrollX: true,
+            // scrollX: true,
             ajax: "{{ route('data.program') }}",  // Update the route for kaitan_sdg data
             processing: true,
             serverSide: true,
@@ -173,7 +173,7 @@ $(document).ready(function() {
                 {data: "tanggalmulai", orderable: true, searchable: true},
                 {data: "tanggalselesai", orderable: true, searchable: true},
                 {data: "totalnilai", orderable: true, searchable: true, visible: false},
-                {data: "ekspektasipenerimamanfaat", orderable: true, searchable: true, visible: false}, 
+                {data: "ekspektasipenerimamanfaat", orderable: true, searchable: true, visible: false},
                 {data: "ekspektasipenerimamanfaatwoman", orderable: true, searchable: true, visible: false},
                 {data: "ekspektasipenerimamanfaatman", orderable: true, searchable: true, visible: false},
                 {data: "ekspektasipenerimamanfaatgirl", orderable: true, searchable: true, visible: false},
@@ -184,20 +184,20 @@ $(document).ready(function() {
                 {
                     data: "status",
                     orderable: true,
-                    searchable: true,
+                    searchable: true, width: "10%",
                     className: "text-center",
                     render: function (data, type, row) {
-                        if (data == 'Draft') {
+                        if (data === 'draft') {
                             return '<span class="badge badge-secondary">Draft</span>';
-                        } else if (data == 'Pending') {
+                        } else if (data === 'pending') {
                             return '<span class="badge badge-warning">Pending</span>';
-                        } else if (data == 'Submit') {
+                        } else if (data === 'submit') {
                             return '<span class="badge badge-success">Submit</span>';
                         }
                         return data;  // Return data if none of the conditions are met
                     }
                 },
-                
+
                 { data: "action", className: "text-center", orderable: false }
             ],
             layout: {
@@ -258,7 +258,7 @@ $(document).ready(function() {
         //     let id = $('#id').val();
         //     let url = '{{ route('program.update', ':id') }}'.replace(':id', id);
         //     let formData = $(this).serialize();
-            
+
         //     $.ajax({
         //         url: url,
         //         method: 'PUT',
