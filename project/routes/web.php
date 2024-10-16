a69cf0e731d4832e7988d540de6f328bdc01f45a
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-10-16 16:21:09 +0800

    #41 - keterengan route

--- project/routes/web.php
+++ project/routes/web.php
@@ -184,8 +184,8 @@ Route::middleware(['auth'])->group(function () {
     //Route Program by Siva
     //Program
     Route::get('data/program', [ProgramController::class, 'getData'])->name('data.program');
-    Route::get('program/api/pendonor/{id}', [ProgramController::class, 'getProgramPendonor'])->name('api.program.pendonor');
-    Route::get('program/api/donor', [ProgramController::class, 'getProgramDonor'])->name('api.program.donor');
+    Route::get('program/api/pendonor/{id}', [ProgramController::class, 'getProgramPendonor'])->name('api.program.pendonor'); // fill form from selected pendonor
+    Route::get('program/api/donor', [ProgramController::class, 'getProgramDonor'])->name('api.program.donor'); // get all pendonor data
     Route::get('program/api/staff', [ProgramController::class, 'getProgramStaff'])->name('api.program.staff');
     Route::get('program/api/lokasi', [WilayahController::class, 'getProgramLokasi'])->name('api.program.lokasi'); //temporary , use wirawan card ?
     Route::resource('program', ProgramController::class);
