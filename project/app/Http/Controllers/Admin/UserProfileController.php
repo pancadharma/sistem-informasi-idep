9312410090db5f3583843ca56b31151be04d7c77
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-10-22 13:15:20 +0800

    #update user profile route

--- project/app/Http/Controllers/Admin/UserProfileController.php
+++ project/app/Http/Controllers/Admin/UserProfileController.php
@@ -8,34 +8,63 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateProfileRequest;
 use Illuminate\Database\QueryException;
 use App\Http\Requests\UpdatePasswordRequest;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\DB;
 
 class UserProfileController extends Controller
 {
+    // public function showProfile($identifier)
+    // {
+    //     $user = User::where('username', $identifier)->first();
+    //     if (!$user) {
+    //         $user = User::findOrFail($identifier);
+    //     }
+
+    //     return view('master.users.profile.update', compact('user'));
+    // }
     public function showProfile($identifier)
     {
-        $user = User::where('username', $identifier)->first();
-        if (!$user) {
-            $user = User::findOrFail($identifier);
+        $user = null;
+        if (auth()->check()) {
+            // Fetch the user by username
+            $user = User::where('username', $identifier)->first();
+            // Fetch the user by ID if not found by username
+            if (!$user) {
+                $user = User::find($identifier);
+            }
+            // Check if the authenticated user is trying to access their own profile or another user's profile
+            if ($user && (auth()->user()->id != $user->id || auth()->user()->username != $user->username)) {
+                // abort(403, 'Unauthorized access to other user profile');
+                return redirect()->route('profile.show', ['identifier' => auth()->user()->username]);
+            }
+            // Handle user not found scenario
+            if (!$user) {
+                return redirect()->route('profile.show', ['identifier' => auth()->user()->username]);
+                // abort(404, 'User Not Found');
+            }
+        } else {
+            abort(403, 'Dilarang access');
+            return redirect()->route('profile.show', ['identifier' => auth()->user()->username]);
         }
-
         return view('master.users.profile.update', compact('user'));
     }
     public function show($identifier)
     {
-        $user = auth()->user();
-        // if (auth()->check() && auth()->user()->id ?? auth()->user()->username == $identifier)
-        if ($user->id != $identifier && $user->username != $identifier) {
-            // Redirect to their own profile if attempting to access someone else's data
-            return redirect()->route('profile.show', ['username' => $user->username]);
-        }
-        $user = User::where('username', $identifier)->first();
-        if (!$user) {
-            $user = User::findOrFail($identifier);
-        }
-        return view('master.users.profile.update', compact('user'));
+        return redirect()->route('profile.show', ['identifier' => auth()->user()->username]);
+        //     $user = auth()->user();
+        //     // // if (auth()->check() && auth()->user()->id ?? auth()->user()->username == $identifier)
+        //     // if ($user->id != $identifier && $user->username != $identifier) {
+        //     //     // Redirect to their own profile if attempting to access someone else's data
+        //     //     return redirect()->route('profile.show', ['username' => $user->username]);
+        //     // }
+        //     // $user = User::where('username', $identifier)->first();
+        //     // if (!$user) {
+        //     //     $user = User::findOrFail($identifier);
+        //     // }
+        //     // return view('master.users.profile.update', compact('user'));
     }
+
 
     public function update(UpdateProfileRequest $request)
     {
@@ -66,8 +95,8 @@ class UserProfileController extends Controller
                 $filename = str_replace(' ', '_', $user->username) . '.' . $request->file('profile_picture')->getClientOriginalExtension();
                 $user->clearMediaCollection($user->username);
                 $user->addMediaFromRequest('profile_picture')
-                ->usingFileName($filename)
-                ->toMediaCollection($user->username, 'userprofile');
+                    ->usingFileName($filename)
+                    ->toMediaCollection($user->username, 'userprofile');
             }
             $user->save();
 
