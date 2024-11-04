23e2762b0dcb51666970c9300b271ca451d280fb
Wira <yanwirawan33@gmail.com>
2024-11-04 12:05:56 +0800

    #60 & #77
    Penyesuaian tampilan pada form

--- project/resources/views/tr/program/js/detail-create/reportschedule.blade.php
+++ project/resources/views/tr/program/js/detail-create/reportschedule.blade.php
@@ -12,7 +12,6 @@
         `<div class="row" id=${i}>
             <div class="col-lg-3 form-group">
                 <div class="input-group">
-                    <label for="pendonor_id" class="input-group small mb-2 mt-2">Tanggal</label>
                     <input type="date" id="tanggal" name="tanggallaporan[]" class="form-control">
                 </div>
             </div>
@@ -20,7 +19,6 @@
             
             <div class="col form-group">
                 <div class="input-group">
-                    <label for="detail" class="input-group small mb-2 mt-2">Keterangan</label>
                     <textarea name="keteranganlaporan[]" class="form-control" placeholder="keterangan" rows="1"></textarea>
                     <span class="input-group-append ml-2" >
                         <button type="button" class="btn btn-danger form-control remove-pendonor nilaidonasi btn-flat" data-target="${i}"><i class="bi bi-trash"></i></button>
