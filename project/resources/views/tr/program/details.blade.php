9b682e61a65a2f4edaf08d9303511153ad0be88c
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-11-04 17:23:45 +0800

    disable summer note JS to change activty input text

--- project/resources/views/tr/program/details.blade.php
+++ project/resources/views/tr/program/details.blade.php
@@ -417,22 +417,22 @@
 
             // Initialize Summernote for the new textareas
 
-            $(`#has-activity-${activityIndex} textarea`).each(function() {
-                if (!$(this).data('initialized')) {
-                    $(this).summernote({
-                        height: 100,
-                        width: '100%',
-                        toolbar: [
-                            ['style', ['bold', 'italic']],
-                            ['color', ['color']],
-                            ['paragraph', ['paragraph']],
-                            ['view', ['fullscreen', 'codeview']],
-                        ],
-                        inheritPlaceholder: true,
-                    });
-                    $(this).data('initialized', true); // Mark this textarea as initialized
-                }
-            });
+            // $(`#has-activity-${activityIndex} textarea`).each(function() {
+            //     if (!$(this).data('initialized')) {
+            //         $(this).summernote({
+            //             height: 100,
+            //             width: '100%',
+            //             toolbar: [
+            //                 ['style', ['bold', 'italic']],
+            //                 ['color', ['color']],
+            //                 ['paragraph', ['paragraph']],
+            //                 ['view', ['fullscreen', 'codeview']],
+            //             ],
+            //             inheritPlaceholder: true,
+            //         });
+            //         $(this).data('initialized', true); // Mark this textarea as initialized
+            //     }
+            // });
         });
 
         // Event delegation to handle removing activity rows
