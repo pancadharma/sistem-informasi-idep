a8507106e80bf91c00e83caf70d69b71764e182e
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-11-13 15:08:56 +0800

    #71 - update migration files trkegiatan,

--- project/database/migrations/2024_11_07_111852_create_kegiatans_table.php
+++ project/database/migrations/2024_11_07_111852_create_kegiatans_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+     public function up(): void
     {
         Schema::create('trkegiatan', function (Blueprint $table) {
             $table->id();
@@ -17,34 +17,38 @@ return new class extends Migration
             $table->string('kode', 50);
             $table->string('nama', 500);
             $table->dateTime('tanggalmulai');
-            $table->dateTime('tanggalselesai');
+            $table->dateTime('tanggalselesai')->nullable();
             $table->foreignId('dusun_id')->constrained('dusun')->onDelete('cascade');
-            $table->decimal('long', 9, 6);
-            $table->decimal('lat', 9, 6);
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
             $table->foreignId('kategorilokasikegiatan_id')->constrained('mkategorilokasikegiatan')->onDelete('cascade');
-            $table->string('tempat', 200);
-            $table->string('deskripsi', 500)->nullable();
-            $table->string('tujuan', 500)->nullable();
-            $table->string('yangterlibat', 500)->nullable();
+            $table->string('tempat', 500)->nullable();
+            $table->longText('deskripsi')->nullable();
+            $table->longText('tujuan')->nullable();
+            $table->longText('yangterlibat')->nullable();
             $table->string('pelatih', 500)->nullable();
-            $table->string('informasilain', 500)->nullable();
+            $table->longText('informasilain')->nullable();
             $table->string('luaslahan', 50)->nullable();
             $table->foreignId('jenisbantuan_id')->nullable()->constrained('mjenisbantuan')->onDelete('cascade');
             $table->foreignId('satuan_id')->nullable()->constrained('msatuan')->onDelete('cascade');
-            $table->string('tindaklanjut', 500)->nullable();
-            $table->string('tantangan', 500)->nullable();
-            $table->string('rekomendasi', 500)->nullable();
+            $table->longText('tindaklanjut')->nullable();
+            $table->longText('tantangan')->nullable();
+            $table->longText('rekomendasi')->nullable();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->string('status', 50);
+            $table->string('status', 50)->default('draft')->nullable();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('trkegiatan');
+        Schema::enableForeignKeyConstraints();
     }
+
 };
