135dadb23a19446acdf1ecb3a39fbe1d3abb79d3
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-11-15 10:54:32 +0800

    #resolve conflict wrong PR to
    update migration that has been pushed into main instead of to dev

--- project/database/migrations/2024_11_07_111852_create_kegiatans_table.php
+++ project/database/migrations/2024_11_07_111852_create_kegiatans_table.php
@@ -67,9 +67,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('trkegiatan');
-    }
-
         Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('trkegiatan');
         Schema::enableForeignKeyConstraints();
