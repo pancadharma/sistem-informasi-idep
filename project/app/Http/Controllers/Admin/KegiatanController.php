d9e1f5e7bd524a2dbfe16e62e2b73eb5b910c95b
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2024-11-23 14:11:45 +0800

    #78 - improvement

--- project/app/Http/Controllers/Admin/KegiatanController.php
+++ project/app/Http/Controllers/Admin/KegiatanController.php
@@ -19,44 +19,6 @@ class KegiatanController extends Controller
         abort_if(Gate::denies('kegiatan_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         return view('tr.kegiatan.index');
     }
-    // public function list_kegiatan(Request $request)
-    // {
-    //     if (!$request->ajax() && !$request->isJson()) {
-    //         return "Not an Ajax Request & JSON REQUEST";
-    //     }
-
-    //     $kegiatan = Kegiatan::with(['dusun', 'users', 'kategori_lokasi', 'activity.program_outcome_output.program_outcome.program', 'satuan', 'jenis_bantuan'])
-    //         ->select('trkegiatan.*')
-    //         ->get()
-    //         ->map(function ($item) {
-    //             $item->tanggalselesai = Carbon::parse($item->tanggalselesai)->format('d-m-Y'); // Format the date as needed
-    //             return $item;
-    //         });
-    //     $data = DataTables::of($kegiatan)
-    //         ->addIndexColumn()
-    //         ->addColumn('program_name', function ($kegiatan) {
-    //             return $kegiatan->activity->program_outcome_output->program_outcome->program->nama ?? 'N/A';
-    //         })
-    //         ->addColumn('action', function ($kegiatan) {
-    //             $buttons = [];
-
-    //             if (auth()->user()->id === 1 || auth()->user()->can('kegiatan_edit')) {
-    //                 $buttons[] = $this->generateButton('edit', 'info', 'pencil-square', __('cruds.kegiatan.edit') . '&nbsp;' . $kegiatan->nama, $kegiatan->id);
-    //             }
-    //             if (auth()->user()->id === 1 || auth()->user()->can('kegiatan_view') || auth()->user()->can('kegiatan_access')) {
-    //                 $buttons[] = $this->generateButton('view', 'primary', 'folder2-open', __('cruds.kegiatan.show') . '&nbsp;' . $kegiatan->nama, $kegiatan->id);
-    //             }
-    //             if (auth()->user()->id === 1 || auth()->user()->can('kegiatan_details_edit') || auth()->user()->can('kegiatan_edit')) {
-    //                 $buttons[] = $this->generateButton('details', 'danger', 'list-ul', __('cruds.kegiatan.details') . '&nbsp;' . $kegiatan->nama, $kegiatan->id);
-    //             }
-    //             return "<div class='button-container'>" . implode(' ', $buttons) . "</div>";
-    //         })
-    //         ->rawColumns(['action'])
-    //         ->make(true);
-
-    //     return $data;
-    // }
-
     public function list_kegiatan(Request $request)
     {
         if (!$request->ajax() && !$request->isJson()) {
@@ -67,14 +29,16 @@ class KegiatanController extends Controller
             ->select('trkegiatan.*')
             ->get()
             ->map(function ($item) {
-                $item->tanggalmulai = Carbon::parse($item->tanggalmulai)->format('d-m-Y'); // Customize the format as needed
-                $item->tanggalselesai = Carbon::parse($item->tanggalselesai)->format('d-m-Y'); // Customize the format as needed
-                $item->duration_in_days = $item->getDurationInDays();
+            // Calculate duration before formatting
+            $item->duration_in_days = $item->getDurationInDays();
 
-                // Add calculated values
-                $program = $item->activity->program_outcome_output->program_outcome->program;
-                $item->total_beneficiaries = $program->getTotalBeneficiaries();
-                $item->duration_in_days = $program->getDurationInDays();
+            // Format dates after calculating duration
+            $item->tanggalmulai = Carbon::parse($item->tanggalmulai)->format('d-m-Y');
+            $item->tanggalselesai = Carbon::parse($item->tanggalselesai)->format('d-m-Y');
+
+            // Add calculated values
+            $program = $item->activity->program_outcome_output->program_outcome->program;
+            $item->total_beneficiaries = $program->getTotalBeneficiaries();
 
                 return $item;
             });
