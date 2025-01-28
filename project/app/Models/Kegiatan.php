2d6a1965940671510259517f32968ca116412f76
Wira <yanwirawan33@gmail.com>
2025-01-28 13:28:19 +0800

    69 - DONE logactivity
    - fix some error after merger

--- project/app/Models/Kegiatan.php
+++ project/app/Models/Kegiatan.php
@@ -8,8 +8,10 @@ use App\Traits\Auditable;
 use Spatie\Image\Enums\Fit;
 use Spatie\MediaLibrary\HasMedia;
 
+use Spatie\Activitylog\LogOptions;
 use GedeAdi\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
