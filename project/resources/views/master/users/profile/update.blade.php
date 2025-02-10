cddae229702a79bb68c41a78435906bf4135b6c4
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2025-02-10 17:21:29 +0800

    fix profile has no jabatan in sidebar and edit pengguna

--- project/resources/views/master/users/profile/update.blade.php
+++ project/resources/views/master/users/profile/update.blade.php
@@ -9,33 +9,16 @@
     <div class="col-md-3">
         <div class="card card-primary card-outline">
             <div class="card-body box-profile">
-                <!-- <div class="text-center">
-                    <img class="profile-pic img-fluid img-circle" src="{{ $user->adminlte_image() }}?t={{ time() }}"
-                        alt="{{ $user->nama ?? '' }} profile picture">
-                </div> -->
                 <div class="text-center">
                     <a class="img-url" href="{{ $user->full_profile() }}?t={{ time() }}" data-lightbox="profile" target="_blank"
                         data-title="{{ $user->nama ?? '' }} Profile Picture">
-                        <img id="profilePic" class="profile-pic img-fluid img-circle"
-                            src="{{ $user->adminlte_image() }}?t={{ time() }}"
-                            alt="{{ $user->nama ?? '' }} profile picture">
+                        <img id="profilePic" class="profile-pic img-fluid img-circle" src="{{ $user->adminlte_image() }}?t={{ time() }}" alt="{{ $user->nama ?? '' }} profile picture">
                     </a>
                 </div>
 
                 <h3 class="profile-username text-center">{{ $user->nama }}</h3>
-                <p class="text-muted text-center">{{ $user->jabatans->nama }}</p>
-                <!-- <ul class="list-group list-group-unbordered mb-3">
-                    <li class="list-group-item">
-                        <b>Followers</b> <a class="float-right">1,322</a>
-                    </li>
-                    <li class="list-group-item">
-                        <b>Following</b> <a class="float-right">543</a>
-                    </li>
-                    <li class="list-group-item">
-                        <b>Friends</b> <a class="float-right">13,287</a>
-                    </li>
-                </ul> -->
-                <!-- <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a> -->
+                <p class="text-muted text-center">{{ $user->jabatans->nama ??  '-' }}</p>
+
             </div>
 
         </div>
