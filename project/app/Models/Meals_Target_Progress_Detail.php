5be0515da364f83811c2bbb57009225bebc2efc9
Dona <adi.yudhanegaradona@gmail.com>
2025-04-23 21:52:04 +0800

    setup: model/migration untuk meals target_progress & target_progress_detail

---
+++ project/app/Models/Meals_Target_Progress_Detail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meals_Target_Progress_Detail extends Model
+{
+    use HasFactory;
+
+    protected $table = "trmeals_target_progress_detail";
+}
