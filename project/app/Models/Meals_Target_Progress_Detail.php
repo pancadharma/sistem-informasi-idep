fb57d3113512ec7a8488257c4cf0ac0e92b3a8ce
Dona <adi.yudhanegaradona@gmail.com>
2025-05-07 05:06:17 +0800

    add: bensampo/laravel-enum to handle enumeration with localization setup: risk & status enum for TargetProgressDetail setup: risk & status select2 with ajax

--- project/app/Models/Meals_Target_Progress_Detail.php
+++ project/app/Models/Meals_Target_Progress_Detail.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\TargetProgressStatus as StatusOptions;
+use App\Enums\TargetProgressRisk as RiskOptions;
 
 class Meals_Target_Progress_Detail extends Model
 {
     use HasFactory;
 
     protected $table = "trmeals_target_progress_detail";
+    protected $casts = [
+        'status'    => StatusOptions::class,
+        'risk'      => RiskOptions::class,
+    ];
 }
