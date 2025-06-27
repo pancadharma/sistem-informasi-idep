d38bf083711f8c7c5534a350c35ddd90caffcdcf
gedeadi <gedeadiputra31@gmail.com>
2025-06-28 01:38:25 +0800

    feat(kegiatan): add dynamic Google Map integration for location management

--- project/resources/views/tr/kegiatan/tabs/basic.blade.php
+++ project/resources/views/tr/kegiatan/tabs/basic.blade.php
@@ -209,7 +209,9 @@
             {{ __('Get Coordinate') }}
             <i class="bi bi-map-fill"></i>
         </label>
-        <div id="map" class="form-control col-form-label"></div>
+        {{-- <div id="map" class="form-control col-form-label"></div> --}}
+        @include('tr.kegiatan._google_map_create')
+
     </div>
 </div>
 
