b8c79936c429f2873603150967bd5f8431e63034
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2025-12-20 20:03:54 +0800

    feat: Add initial database schema for idepdev and BTOR English language file.

---
+++ project/lang/en/btor.php
@@ -0,0 +1,111 @@
+<?php
+
+return [
+    'btor' => 'BACK TO OFFICE REPORT',
+    'basic_information' => 'BASIC INFORMATION',
+
+
+
+    'tgl_pelaporan' => 'Reporting Date',
+    'fase_pelaporan' => 'Reporting Phase',
+    'fase' => 'Phase',
+    'status' => 'Status',
+    'tempat' => 'Location',
+    'programs' => 'Programs',
+    'program' => 'Program',
+    'nama_program' => 'Program Name',
+    'kode_program' => 'Program Code',
+    'nama_kegiatan' => 'Activity Name',
+    'kode_kegiatan' => 'Activity Code',
+    'activity' => 'Activity',
+    'jenis_kegiatan' => 'Activity Type',
+    'sektor_kegiatan' => 'Activity Sector',
+    'mitra_kegiatan' => 'Activity Partner',
+    'pihak_yang_terlibat' => 'Involved Parties',
+    'lokasi_kegiatan' => 'Activity Location',
+    'tanggal_mulai' => 'Start Date',
+    'hari'  => 'days',
+    'tanggal_selesai' => 'End Date',
+
+    'periode' => 'Period',
+    'keterangan' => 'Remarks',
+    'created_at' => 'Created At',
+    'updated_at' => 'Updated At',
+    'lokasi' => 'Location',
+    'nama' => 'Name',
+    'alamat' => 'Address',
+    'tahun_anggaran' => 'Fiscal Year',
+    'kode_budget' => 'Budget Code',
+
+
+    'organization' => 'Organization',
+    'departemen' => 'Department',
+
+    'activity_details' => 'Activity Details',
+    'activity_description' => 'Activity Description',
+
+    'latar_belakang_kegiatan' => 'Activity Background',
+    'no_background_activity' => 'No Activity Background or Not Inputted',
+    'tujuan_kegiatan' => 'Activity Objectives',
+    'no_tujuan_activity' => 'No Activity Objectives or Not Inputted',
+    'keluaran_kegiatan' => 'Activity Outputs',
+    'no_keluaran_activity' => 'No Activity Outputs or Not Inputted',
+    'detail_kegiatan' => 'Activity Details',
+    'deskripsilatarbelakang' => 'Background Description',
+    'deskripsitujuan' => 'Objectives Description',
+    'deskripsikeluaran' => 'Outputs Description',
+    'deskripsiyangdikaji' => 'Assessment',
+
+    'detail_kegiatan_spesifik' => 'Specific Activity Details',
+    'hasil_kegiatan' => 'Activity Results',
+
+    'jumlah_partisipan' => 'Number of Participants Involved and Disaggregated',
+    'table_partisipan' => 'Disaggregation Table by Age and Gender',
+    'table_kelompok_khusus' => 'Disaggregation Table by Special Groups',
+    'penyandang_disabilitas' => 'Persons with Disabilities',
+    'non_disabilitas' => 'Non-disabilities',
+    'non_disabilitas' => 'Other Marginalized Groups',
+    
+    
+    'penerima_manfaat' => 'Beneficiaries',
+
+    
+    'perempuan' => 'Female',
+    'laki_laki' => 'Male',
+    'lainnya' => 'Others',
+    'sub_total' => 'Sub Total',
+    'total_keseluruhan' => 'Grand Total',
+    'orang' => 'Person',
+
+    'dewasa' => 'Adult',
+    'lansia' => 'Elderly',
+    'remaja' => 'Youth',
+    'anak' => 'Child',
+    'grand_total' => 'Grand Total',
+
+    'umur_18_kebawah' => 'Age 18 and below',
+    'umur_18_24' => 'Age 18 - 24 years',
+    'umur_25_59' => 'Age 25 - 59 years',
+    'umur_60' => 'Age 60 years', 
+    'umur_60_ke_atas' => 'Age 60 and above',
+    
+    
+
+    'hasil_pertemuan' => 'Meeting Results',
+
+    'informasi_lokasi' => 'Location Information',
+
+    'prepared_by' => 'Prepared By',
+    'penulis_jabatan' => 'Position',
+    'penulis_laporan' => 'Report Writer',
+    'penanggung_jawab' => 'Person in Charge',
+    'penanggung_jawab_jabatan' => 'Person in Charge Position',
+    'approve_by' => 'Approved By',
+
+    'report_detail' => 'Report Detail',
+    'btor_report_detail' => 'BTOR Report Detail',
+    'btor_report_list' => 'BTOR Report List',
+    'back_to_list' => 'Back to List',
+    'export_pdf' => 'Export PDF',
+    'print_preview' => 'Print Preview',
+];
