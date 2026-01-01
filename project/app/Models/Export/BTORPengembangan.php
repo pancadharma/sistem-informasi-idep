aac42b61a7b2164da510202a66cda9fd41672944
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2026-01-01 14:29:05 +0800

    feat: Add BTOR export models for Pelatihan, Pembelanjaan, Pemetaan, Assessment, Kampanye, Konsultasi, Lainnya, and Monitoring activities.

--- project/app/Models/Export/BTORPengembangan.php
+++ project/app/Models/Export/BTORPengembangan.php
@@ -13,14 +13,14 @@ class BTORPengembangan extends BTOR
         }
 
         return [
-            'jenis_komponen' => $kegiatan->pengembangan->pengembanganjeniskomponen,
-            'berapa_komponen' => $kegiatan->pengembangan->pengembanganberapakomponen,
-            'lokasi_komponen' => $kegiatan->pengembangan->pengembanganlokasikomponen,
-            'yang_terlibat' => $kegiatan->pengembangan->pengembanganyangterlibat,
-            'rencana' => $kegiatan->pengembangan->pengembanganrencana,
-            'kendala' => $kegiatan->pengembangan->pengembangankendala,
-            'isu' => $kegiatan->pengembangan->pengembanganisu,
-            'pembelajaran' => $kegiatan->pengembangan->pengembanganpembelajaran,
+            'jenis_komponen'     => $kegiatan->pengembangan->pengembanganjeniskomponen,
+            'berapa_komponen'    => $kegiatan->pengembangan->pengembanganberapakomponen,
+            'lokasi_komponen'    => $kegiatan->pengembangan->pengembanganlokasikomponen,
+            'yang_terlibat'      => $kegiatan->pengembangan->pengembanganyangterlibat,
+            'rencana'            => $kegiatan->pengembangan->pengembanganrencana,
+            'kendala'            => $kegiatan->pengembangan->pengembangankendala,
+            'isu'                => $kegiatan->pengembangan->pengembanganisu,
+            'pembelajaran'       => $kegiatan->pengembangan->pengembanganpembelajaran,
         ];
     }
 }
