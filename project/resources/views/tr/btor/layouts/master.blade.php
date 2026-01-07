80faa8b3b39bad861f38d17c52e8bb0ebd6556be
I Gede Adi Surya Eka Pramana Putra <gedeeinstein@gmail.com>
2026-01-08 02:49:15 +0800

    feat: Implement BTOR report views for bulk printing, single display, and associated layouts.

--- project/resources/views/tr/btor/layouts/master.blade.php
+++ project/resources/views/tr/btor/layouts/master.blade.php
@@ -13,7 +13,7 @@
 
     <style>
         body {
-            font-family: 'Arial', sans-serif;
+            font-family: 'Figtree', sans-serif;
             background-color: #f8f9fa;
         }
         .card {
